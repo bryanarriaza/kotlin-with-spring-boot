c9435fb3195d291b0ecab4fbd5867f61e251298b
Graham Cox <graham@grahamcox.co.uk>
2017-05-24 09:03:48 +0100

    Rich JSON response

--- src/main/kotlin/tutorial/FirstController.kt
+++ src/main/kotlin/tutorial/FirstController.kt
@@ -2,6 +2,7 @@ package tutorial
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
 
 /**
  * Our very first controller
@@ -11,4 +12,16 @@ class FirstController {
     /** The ultimate answer to life, the universe and everything */
     @RequestMapping("/answer")
     fun answer() = 42
+
+    /** Get the details of a user */
+    @RequestMapping("/user")
+    fun getUser() : User {
+        val user = User(
+                username = "grahamcox",
+                screenName = "Graham",
+                email = "grahamcox@example.com",
+                registered = Instant.now()
+        )
+        return user
+    }
 }
