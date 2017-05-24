348f4e0137e651c0143517a31ffa79087b8f100a
Graham Cox <graham@grahamcox.co.uk>
2017-05-24 08:54:24 +0100

    First Controller

--- src/main/kotlin/tutorial/Application.kt
+++ src/main/kotlin/tutorial/Application.kt
@@ -2,6 +2,7 @@ package tutorial
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 /**
@@ -9,7 +10,10 @@ import org.springframework.context.annotation.Configuration
  */
 @EnableAutoConfiguration
 @Configuration
-open internal class Application
+open internal class Application {
+    @Bean
+    open fun controller() = FirstController()
+}
 
 /**
  * Run the application
