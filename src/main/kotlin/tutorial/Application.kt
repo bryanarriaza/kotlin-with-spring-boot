d69ac697e9ff73b7fb8990a20304d575432a6c50
Graham Cox <graham@grahamcox.co.uk>
2017-05-24 08:40:29 +0100

    Initial application setup

---
+++ src/main/kotlin/tutorial/Application.kt
@@ -0,0 +1,20 @@
+package tutorial
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.Configuration
+
+/**
+ * The main entry point to the application
+ */
+@EnableAutoConfiguration
+@Configuration
+open internal class Application
+
+/**
+ * Run the application
+ * @param args The command line arguments
+ */
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
